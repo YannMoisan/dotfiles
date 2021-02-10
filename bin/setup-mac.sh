c2a25e497c3c50f8fae4a11b592c7b551fe9eb03
Yann Moisan <yamo93@gmail.com>
2021-02-10 10:45:30 +0100

    setup fix projects dir

---
+++ bin/setup-mac.sh
@@ -0,0 +1,16 @@
+# Show hidden files ...
+defaults write com.apple.finder AppleShowAllFiles YES
+# Show file extensions by default ...
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+
+# Show path bar ...
+defaults write com.apple.finder ShowPathbar -bool true
+
+# Show status bar ...
+defaults write com.apple.finder ShowStatusBar -bool true
+
+# Display the Full Pathname 
+defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
+
+# Use column view in Finder ...
+defaults write com.apple.Finder FXPreferredViewStyle Nlsv
